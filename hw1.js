e8d32658e374d69b83084847737e36d958bf6cba
Mariana Osorio Lozano <marianaosoriolozano@Marianas-MacBook-Air.local>
2022-04-03 21:40:05 -0500

    final

--- hw1.js
+++ hw1.js
@@ -4,17 +4,19 @@ let questionOne = prompt("What is your name?");
  document.getElementById("blankOne").innerHTML = questionOne;
 
 
-
  let enterDate = prompt("When is your birthday?");
  document.getElementById("enterDate").innerHTML = enterDate;
 
  let sharkName = prompt("What is your favorite thing in the whole entire world?!?!?!?");
  document.getElementById("blankTwo").innerHTML = sharkName;
 
- let gorceriaInAnyIdioma = prompt("What's you favorite curse word? Any language counts :)");
- document.getElementById("blankThree").innerHTML = gorceriaInAnyIdioma; 
+ let gorceriaInAnyIdioma = prompt("What is you favorite curse word? Any language counts :)");
+ document.getElementById("blankThree").innerHTML = gorceriaInAnyIdioma + "!!!!!!!";
 
- let foodForGood = prompt("What's your favorite food?");
+ let questionName = prompt("What is your name, again?");
+ document.getElementById("blankFive").innerHTML = questionName;
+
+ let foodForGood = prompt("What is your favorite food?");
  document.getElementById("dessert").innerHTML = foodForGood;
 
 
