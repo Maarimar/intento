01f4694b118491d419f3387d75d68b8d691436b6
Mariana Osorio Lozano <marianaosoriolozano@Marianas-MacBook-Air.local>
2022-04-03 20:33:14 -0500

    cambios pequenitos

--- hw1.js
+++ hw1.js
@@ -1,5 +1,22 @@
-document.getElementById("mytitle").innerHTML =
-"Printng Titles and Text with ID";
 
-document.getElementById("content").innerHTML =
-"Bear claw icing carrot cake chocolate oat cake carrot cake. Candy canes brownie topping pastry pastry topping chocolate bar. Chocolate chupa chups bear claw bear claw cupcake apple pie tiramisu topping apple pie. Donut tiramisu dragée chocolate biscuit. Shortbread biscuit lollipop tiramisu tiramisu jelly-o shortbread pie. Halvah tart biscuit chocolate cake danish carrot cake donut chocolate bar. Dessert powder icing sugar plum donut cupcake jelly beans. Bonbon wafer halvah chupa chups chocolate cake dessert pastry. Croissant caramels lemon drops caramels carrot cake lollipop jelly-o. Bear claw sugar plum wafer jujubes apple pie gingerbread fruitcake. Dragée topping tart caramels macaroon gummi bears. Oat cake caramels bear claw toffee muffin marshmallow. Apple pie carrot cake pudding lollipop cookie."
+
+let questionOne = prompt("What is your name?");
+ document.getElementById("blankOne").innerHTML = questionOne;
+
+
+
+ let enterDate = prompt("When is your birthday?");
+ document.getElementById("enterDate").innerHTML = enterDate;
+
+ let sharkName = prompt("What is your favorite thing in the whole entire world?!?!?!?");
+ document.getElementById("blankTwo").innerHTML = sharkName;
+
+ let gorceriaInAnyIdioma = prompt("What's you favorite curse word? Any language counts :)");
+ document.getElementById("blankThree").innerHTML = gorceriaInAnyIdioma; 
+
+ let foodForGood = prompt("What's your favorite food?");
+ document.getElementById("dessert").innerHTML = foodForGood;
+
+
+
+
